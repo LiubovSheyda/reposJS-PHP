89c2b60bc648fcab8ac8c5db418225b714a1e4c0
LiubovSheyda <liubovsheyda@gmail.com>
2020-03-20 15:04:22 +0700

    Update script.js

--- regular_expressions/lesson18/script.js
+++ regular_expressions/lesson18/script.js
@@ -1,17 +1,26 @@
 let str1 = 'http://cat.jpg';
 let str2 = 'ссылка на кота с расширением jpeg.';
 //18.2
+
+// лишние символы в выражении
+
 console.log('Начинается ли ' + str1 + ' с https:// или http:// ? ' + /^[https://]|^[http://]/.test(str1));
 console.log('Начинается ли ' + str2 + ' с https:// или http:// ? ' + /^[https://]|^[http://]/.test(str2));
 //18.4
 console.log('Заканчиается ли ' + str1 + ' jpeg или jpg ? ' + /jpeg$|jpg$/.test(str1));
 console.log('Заканчиается ли ' + str2 + ' jpeg или jpg ? ' + /jpeg$|jpg$/.test(str2));
 //18.6
+
+// длину необходимо указать в выражении
+
 str1 = '123456789';
 console.log('Является ли ' + str1 + ' числом длиной до 12 цифр ? ' + (/\d/.test(str1) && str1.length<12 && str1.length>=1));
 str1 = '123456789123456789';
 console.log('Является ли ' + str1 + ' числом длиной до 12 цифр ? ' + (/\d/.test(str1) && str1.length<12 && str1.length>=1));
 //18.8
+
+// все реализовать через регулярку
+
 str1 = '11.11.2000';
 let str = str1.split('.');
 console.log('Является ли ' + str1 + ' датой в формате день.месяц.год ? ' + (/\d.\d.\d/.test(str1)
@@ -21,6 +30,9 @@ str = str1.split('.');
 console.log('Является ли ' + str1 + ' датой в формате день.месяц.год ? ' + (/\d.\d.\d/.test(str1)
 && str[0]<32 && str[0]>=1 && str[1]<13 && str[1]>=1 && str[0]<2100 && str[0]>=0));
 //18.10
+
+// проверить ВСЕ email через регулярку, универсальное выражение
+
 str1 = 'niktoto@gmail.com';
 console.log('Является ли ' + str1 + ' корректным email`ом ? ' + (/@gmail.com$|@mail.ru|@ya.ru$$/.test(str1)));
 str1 = 'niktoto@mail.com';
